735200ed2fa27f4cb4cadcc4f29affff99025241
VinceVence <vencer.lanzvincent09@gmail.com>
2024-08-30 09:54:04 +0800

    [ADD] module versions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly
 
 st.title("Hello World!")
+
+# Print versions
+st.write(f"Streamlit version: {st.__version__}")
+st.write(f"Pandas version: {pd.__version__}")
+st.write(f"NumPy version: {np.__version__}")
